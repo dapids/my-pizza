ad20fa457eefd4836e32dacabcc576bce6e2acb3
David Sorrentino <me@davidsorrentino.com>
2025-01-31 11:29:16 +0100

    Suggest the flour amount starting from the number of people

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ const roundToNearestTen = (grams: number) => Math.round(grams / 10) * 10;
 const getWaterGrams = (flourGrams: number) => flourGrams * 0.7;
 const getSaltGrams = (flourGrams: number) => Math.ceil(flourGrams * 0.02);
 const getYestGrams = (flourGrams: number) => Math.ceil(flourGrams * 0.012);
+
 const humanizeTeaspoons = (teaspoons: number) => {
   const amounts = [{
     amount: '',
@@ -49,13 +50,27 @@ const humanizeTeaspoons = (teaspoons: number) => {
 }
 
 export default function Home() {
-  const defaultFlourGrams = 300;
-  const [flourGrams, setFlourGrams] = useState(defaultFlourGrams);
+  const defaultFlourGrams = 350;
+  const [flourGramsSelected, setFlourGramsSelected] = useState(defaultFlourGrams);
 
   const onFlourGramsChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setFlourGrams(Number(event.target.value));
+    setFlourGramsSelected(Number(event.target.value));
   }, []);
 
+  const defaultPeopleNumber = 2;
+  const [numberOfPeopleSelected, setNumberOfPeopleSelected] = useState(defaultPeopleNumber);
+
+  const onNumberOfPeopleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setNumberOfPeopleSelected(Number(event.target.value));
+  }, []);
+
+  const [peopleSelected, setPeopleSelected] = useState(false);
+
+  const onPeopleSelected = useCallback((selected: boolean) => () => {
+    setPeopleSelected(selected);
+  }, []);
+
+  const flourGrams = peopleSelected ? numberOfPeopleSelected * 175 : flourGramsSelected;
   const flourGramsFirstPart = roundToNearestTen(flourGrams * 0.8);
   const flourGramsSecondPart = roundToNearestTen(flourGrams * 0.1);
   const flourGramsThirdPart = roundToNearestTen(flourGrams * 0.1);
@@ -75,11 +90,32 @@ export default function Home() {
       </header>
 
       <main id={styles.main}>
-        <div id={styles.form}>
-          <div className={styles.input}>
-            <label htmlFor="flour-grams">Grammi di farina</label>
-            <input defaultValue={defaultFlourGrams} id="flour-grams" min="10" onChange={onFlourGramsChange} step="10" type="number" />
-          </div>
+        <div>
+          <h2>Quantità</h2>
+
+          {peopleSelected ? (
+            <div id={styles.form}>
+              <div className={styles.input}>
+                <label htmlFor="number-of-people">Numero di persone</label>
+                <input id="number-of-people" min="1" onChange={onNumberOfPeopleChange} step="1" type="number" value={numberOfPeopleSelected} />
+              </div>
+
+              <p>
+                Preferisci specificare la quantità di farina che vuoi utilizzare? <button onClick={onPeopleSelected(false)}>Clicca qui!</button>
+              </p>
+            </div>
+          ) : (
+            <div id={styles.form}>
+              <div className={styles.input}>
+                <label htmlFor="flour-grams">Grammi di farina</label>
+                <input id="flour-grams" min="50" onChange={onFlourGramsChange} step="50" type="number" value={flourGramsSelected} />
+              </div>
+
+              <p>
+                Preferisci specificare il numero di persone per cui vuoi fare la pizza? <button onClick={onPeopleSelected(true)}>Clicca qui!</button>
+              </p>
+            </div>
+          )}
         </div>
 
         <div>
@@ -96,51 +132,60 @@ export default function Home() {
 
         <div>
           <h2>Preparazione</h2>
-          <p>
-            Sciogliere {yestGrams} grammi di lievito in {warmWaterGrams} grammi di acqua a 28°C.
-          </p>
-          <p>
-            Versare {flourGramsFirstPart} grammi di farina in un lato della madia.
-          </p>
-          <p>
-            Versare {coldWaterGrams} grammi di acqua a 10°C nell&apos;altro lato della madia.
-          </p>
-          <p>
-            Aggiungere {saltGrams} grammi ({saltTeaspoons}) di sale all&apos;acqua nella madia e mescolare con la mano finché il sale non si scioglie.
-          </p>
-          <p>
-            Incorporare la farina un po&apos; alla volta con la mano. La gradualità del processo è importante per evitare la formazione di grumi.
-          </p>
-          <p>
-            Lavorare l&apos;impasto con le mani in modo che prenda aria e mozzarlo (ovvero srtizzarlo tirandolo verso l&apos;alto).
-          </p>
-          <p>
-            Aggiungere l&apos;acqua con il lievito e lavorare finché l&apos;acqua non viene completamente assorbita.
-          </p>
-          <p>
-            Incorporare {flourGramsSecondPart} grammi di farina.
-          </p>
-          <p>
-            Lavorare l&apos;impasto per 5/10 minuti finché non si stacca bene dalla madia.
-          </p>
-          <p>
-            Far riposare l&apos;impasto per 30 minuti.
-          </p>
-          <p>
-            Incorporare {flourGramsThirdPart} grammi di farina.
-          </p>
-          <p>
-            Realizzare delle pieghe finché l&apos;impasto è incordiato, ovvero fino a quando appare liscio, omogeneo ed elastico.
-          </p>
-          <p>
-            Lasciare riposare l&apos;impasto in frigo per 12-18 ore.
-          </p>
-          <p>
-            Stagliare l&apos;impasto in panetti e farli riposare per 2 ore.
-          </p>
-          <p>
-            Stendere l&apos;impasto, condire la pizza, e infornare.
-          </p>
+
+          <ul className={styles.steps}>
+            <li>
+              Prepariamo {coldWaterGrams} grammi di acqua a 10°C in un recipiente. Per farla raffreddare, possiamo metterla in frigo per 1 ora oppure in freezer per 30 minuti.
+            </li>
+            <li>
+              Sciogliamo {yestGrams} grammi di lievito in {warmWaterGrams} grammi di acqua a 28°C in un recipiente.
+            </li>
+            <li>
+              Versiamo {flourGramsFirstPart} grammi di farina in un lato della madia o della ciotola dove avverrà l&apos;impasto.
+            </li>
+            <li>
+              Versiamo l&apos;acqua a 10°C nell&apos;altro lato della madia.
+            </li>
+            <li>
+              Aggiungiamo il sale all&apos;acqua nella madia e mescoliamo con la mano finché il sale non si scioglie.
+            </li>
+            <li>
+              Incorporiamo la farina un po&apos; alla volta con la mano. La gradualità del processo è importante per evitare la formazione di grumi.
+            </li>
+            <li>
+              Lavoriamo l&apos;impasto con le mani in modo che prenda aria. Durante la lavorazione è importante mozzarlo, ovvero strizzarlo tirandolo verso l&apos;alto.
+            </li>
+            <li>
+              Aggiungiamo l&apos;acqua con il lievito e lavoriamo l&apos;impasto finché l&apos;acqua non viene completamente assorbita.
+            </li>
+            <li>
+              Incorporiamo altri {flourGramsSecondPart} grammi di farina.
+            </li>
+            <li>
+              Lavoriamo l&apos;impasto per 5/10 minuti finché non si stacca bene dalla madia.
+            </li>
+            <li>
+              Lasciamo riposare l&apos;impasto per 30 minuti coprendo la madia o della ciotola con uno strofinaccio.
+            </li>
+            <li>
+              Incorporiamo altri {flourGramsThirdPart} grammi di farina.
+            </li>
+            <li>
+              Realizziamo delle pieghe finché l&apos;impasto non è incordato, ovvero fino a quando appare liscio, omogeneo, ed elastico.
+            </li>
+            <li>
+              Lasciamo riposare l&apos;impasto in frigo per 20-24 ore.
+            </li>
+            <li>
+              Stagliamo l&apos;impasto in panetti e facciamoli riposare per 2 o 3 ore.
+            </li>
+            <li>
+              Stendiamo l&apos;impasto, condiamo la pizza, e inforniamo.
+            </li>
+            <li>
+              Buon appetito!
+            </li>
+          </ul>
         </div>
       </main>
     </div>
